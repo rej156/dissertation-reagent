6a6cef1526ccb87626eb2fb73a5e39db4f4d99de
rej156 <eric.juta@red-badger.com>
2015-01-09 16:33:56 +0000

    Made finite state machine design choices and started scaffolding the main application module

--- src/cljs/test/components/application/application.cljs
+++ src/cljs/test/components/application/application.cljs
@@ -1,2 +1,41 @@
 (ns test.component.application
-  )
+  (:require [test.session :refer [global-put! global-state prefs-state prefs]]
+            [reagent.core :as reagent :refer [atom]]
+            [secretary.core :as secretary]
+            [reagent-forms.core :refer [bind-fields]]))
+
+(defn input [label type id]
+  [:div.id
+   [:label label]
+   [:input.form-control {:field type :id id}]])
+
+(def core-values-state (atom [
+                  {:career {:history '()
+                            :score nil
+                            :vision "I am God."
+                            :goals [
+                                    {:name "First Career Goal"
+                                     :description "Goal description"
+                                     :steps ["First step","Second step"]
+                                     }]
+                            }}
+                  {:finances {}}
+                  {:health {}}
+                  {:family {}}
+                  {:romance {}}
+                  {:personal-growth {}}
+                  {:fun {}}
+                  {:physical-environment {}}
+                  ]))
+
+(defn try-move-next []
+  (set! (.-location js/window) "#/introduction/seventh"))
+
+(def form-template
+  [:div.form-template
+
+   ])
+
+(defn component []
+  [:div.introduction-sixth
+   [bind-fields form-template prefs]])
