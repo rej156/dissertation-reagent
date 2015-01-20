c1414635c048e7169b8d91ea783e5f4afd72fc5d
Eric John Juta <ericjohnjuta@gmail.com>
2015-01-20 15:12:46 +0000

    Made scores mapped out to a vector to populate 3 user options by index values Made notes of what to do next in linear progression

--- src/cljs/test/components/application/application.cljs
+++ src/cljs/test/components/application/application.cljs
@@ -2,6 +2,7 @@
   (:require [test.session :refer [global-put! global-state prefs-state prefs merge-atoms]]
             [reagent.core :as reagent :refer [atom]]
             [secretary.core :as secretary]
+            [instaparse.core :as insta]
             [reagent-forms.core :refer [bind-fields]]))
 
 (defn input [label type id]
@@ -124,20 +125,34 @@
     g = 'c'
     s = 'd'"))
 
-(defn populate-with-no-scores []
-  (loop [core-values @core-values-state]
-    (when (not-empty core-values)
-      (let [current (first core-values)]
+(defn populate-with-no-scores [scores]
+  (loop [remaining-scores scores
+         counter 0]
+    (when (not-empty remaining-scores)
+      (let [current (first remaining-scores)]
         (if (empty? (:history (first (vals current))))
           (if (nil? first-option)
-            (set! first-option (name (first (keys current))))
+            (set! first-option counter)
             (if (nil? second-option)
-              (set! second-option (name (first (keys current)))))
-            (if (nil? third-option)
-              (set! third-option (name (first (keys current))))))))
-      (recur (rest core-values)))))
+              (set! second-option counter)
+              (if (nil? third-option)
+                (set! third-option counter))))))
+      (recur (rest remaining-scores) (inc counter)))))
 
-(defn map-scores-to-vec []
+
+;; (loop [core-values @core-values-state]
+;;     (when (not-empty core-values)
+;;       (let [current (first core-values)]
+;;         (if (empty? (:history (first (vals current))))
+;;           (if (nil? first-option)
+;;             (set! first-option (name (first (keys current))))
+;;             (if (nil? second-option)
+;;               (set! second-option (name (first (keys current)))))
+;;             (if (nil? third-option)
+;;               (set! third-option (name (first (keys current))))))))
+;;       (recur (rest core-values))))
+
+(def map-scores-to-vec
   (-> into []
       (map #(-> %
                 (vals)
@@ -148,10 +163,14 @@
 (defn populate-remaining-with-lowest-scores [scores]
   (loop [scores]
     (when (not-empty scores)
-      (let [current (first scores)]
-        (if ))
-      (recur (rest scores))))
-  )
+      (let [smallest-index (.indexOf scores (min scores))]
+        (if (nil? first-option)
+          (set! first-option smallest-index)
+          (if (nil? second-otion)
+            (set! second-option smallest-index)
+            (if (nil? third-option)
+              (set! third-option smallest-index)))))
+      (recur (rest scores)))))
 
 ;; Do we populate first-options with the indexes of the core values with no
 ;; scores then the smallest scores?
@@ -160,6 +179,44 @@
 ;; recur loop again, if current score is the smallest, populate remaining nil
 ;; options with index value of the current smallest score
 ;; Render the names of the option indexes i.e. (name (first (keys (get @core-values-state first-option))))
+
+;; Now that we've got the indexes of the nil scores and the smallest scores, we
+;; want to work on them. Therefore, we should do the following:
+;; 1) Present the next possible action for that core value to be presented to
+;; the user BUT WITH A RE-RENDER AND PARSING ON EACH USER ACTION
+;; Intermittently, the user is presented to work on the lowest scores at all times.
+;; 2) In these cases, the possible states of action would be...
+;; - [ ] Scoring core value -> Add a "a" string to its (:history (first (vals current)))
+;; **** DO WE UPDATE THE PARSER TO ACCEPT zero or more so it can present the
+;; correct key of the required state? i.e. A score without a goal can still be parsed.
+;; ****
+;; - [ ] Add a vision -> Check if there is a [:V "b"] vector, initiate the
+;; vision adding module then add a "b" to the history string
+;; - [ ] Add/Commit to a goal -> Check if there is a [:G "c" "d" "d"] vector or
+;; a current goal string not being empty,
+;; setting the current option value chosen,
+;; initiate the adding/commiting to a goal module to prompt like the following:
+;; 1) Add a goal to 'XOYO' core value -> Initiate the add a goal module,
+;;  then add the "c" string to the history
+;; 2) Commit to a goal -> Initiate the committing to a goal module, this
+;; displays all the parsed goal names with numbered choice values, setting the
+;; current goal value with the value-index of the chosen option ->
+;; Initiate the add a step module, populate the step name for that chosen goal
+;; and add a "d" to the history string, set the value of the current step as 0
+;; Say that we will remind the user about completing that step periodically
+;; - [ ] Confirm completion of current step name "XOYO" of current goal name
+;; for 'XOYO' core-value->
+;; Initiate the confirm step completion module, prompt the following:
+;; 1) Ask user if they need to add another step, set current step name to new
+;; one and add another "d", incrememnt current step value by 1
+;; 2) Confirm goal completion -> Congratulate them, set current goal string to
+;; empty, set the current step value as nil, ask them to score the core value again
+
+;; ***** Then work on the following:
+;; Recent actions
+;; Mood evaluation and timer
+;; History of actions for a given core value
+;; List of all goals for a given core value
 
 ;; (print first-option)
 ;; (print second-option)
@@ -191,6 +248,6 @@
                      mobile-parser))
    (.log js/console (str "Parsed
   output:" (nth (get (mobile-parser "abcd") 1) 1)))
-   (->> (populate-with-no-scores)
-        (populate-remaining-with-lowest-scores (map-scores-to-vec)))
+   (->> (populate-with-no-scores map-scores-to-vec)
+        (populate-remaining-with-lowest-scores map-scores-to-vec))
    [bind-fields form-template prefs]])
