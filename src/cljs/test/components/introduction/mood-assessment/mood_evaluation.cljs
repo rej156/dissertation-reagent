83f9318a4dba9af3c6e3568747bab24ad6bc1420
Eric John Juta <ericjohnjuta@gmail.com>
2015-02-24 16:43:58 +0000

    Fixed mood evaluation page to use pure reagent instead of Reagent forms

--- src/cljs/test/components/introduction/mood-assessment/mood_evaluation.cljs
+++ src/cljs/test/components/introduction/mood-assessment/mood_evaluation.cljs
@@ -8,48 +8,47 @@
 (def gratitude (atom {}))
 
 (defn try-move-next []
-  (do
-    ;;conj-in [:gratitude []] (merge-atoms prefs gratitude)
-    (set! (.-location js/window) "#/application")
-    ))
-
-(def form-template
-  [:div.row
-   [:div.down-mood {:field :container :visible? #(> 0 (get-in @prefs [:mood :score]))}
-    [:div.col.s12
-     [:h4  "Looks like your mood is a little down."]
-     [:h4 "Would you like to try a little exercise to boost your mood?"]
-     [:div.btn-group {:field :single-select :id :show-gratitude-exercise}
-      [:button.btn.waves-effect.waves-light {:key :yes} "Sure!"]
-      [:button.btn.waves-effect.waves-light {:key :no
-                                             :on-click #(try-move-next)} "Not right now Liz"]]
-     [:div.input-field.col.s12.gratitude-exercise {:field :container :visible? #(= :yes
-                                                                                   (:show-gratitude-exercise
-                                                                                    %))}
-      [:h3 "Let's try this:"]
-      [:h4 "Think of one good thing that happened to you during the past
-  week. Something for which you feel grateful for."]
-      [:br]
-      [:h4 "What is it?*"]
-      [:textarea.materialize-textarea {:field :textarea :id :grateful-event
-                                       :rows 4
-                                       :cols 50}]
-      [:h4 {:for :grateful-reason} "Why are you grateful for it?"]
-      [:textarea.materialize-textarea {:field :textarea :id :grateful-reason
-                                       :rows 4
-                                       :cols 50}]
-      [:br]
-      [:button.btn.waves-effect.waves-light {:on-click #(try-move-next)}
-       "Let's start improving my life Liz!" [:i.mdi-content-send.right]]]]]
-   [:div.mood-good {:field :container :visible? #(<= 0 (get-in @prefs [:mood :score]))}
-    [:h3  "Looks like your mood is good!"]
-    [:button.btn.waves-effect.waves-light {:on-click #(set! (.-location
-                                                             js/window)
-                                                            "#/application")} "Let's start improving my life Liz!" [:i.mdi-content-send.right]]]])
+  (set! (.-location js/window) "#/application"))
 
 (defn component []
   [:div.container
    [:div.row
     [:div.col.s12
      [:h2 (str "Your mood is " (get-in @prefs [:mood :score]) " on a scale between -18 and +18")]]]
-   [bind-fields form-template gratitude]])
+   [:div.row
+    (if (< (get-in @prefs [:mood :score]))
+      [:div.down-mood
+       [:div.col.s12
+        [:h4  "Looks like your mood is a little down."]
+        [:h4 "Would you like to try a little exercise to boost your mood?"]
+        [:div.btn-group
+         [:button.btn.waves-effect.waves-light {:on-click #(swap! gratitude assoc :gratitude-status "yes")}"Sure!"]
+         [:button.btn.waves-effect.waves-light {:on-click #(try-move-next)} "Not right now Liz"]]
+        (if (= "yes" (:gratitude-status @gratitude))
+          [:div.input-field.col.s12.gratitude-exercise
+           [:h3 "Let's try this:"]
+           [:h4 "Think of one good thing that happened to you during the past
+  week. Something for which you feel grateful for."]
+           [:br]
+           [:h4 "What is it?*"]
+           [:textarea.materialize-textarea {:id :grateful-event
+                                            :on-change #(swap! gratitude assoc :gratitude-reason (.. % -target -value))
+                                            :rows 4
+                                            :cols 50}]
+           [:h4 {:for :grateful-reason} "Why are you grateful for it?"]
+           [:textarea.materialize-textarea {:id :grateful-reason
+                                            :on-change #(swap! gratitude assoc :gratitude-text (.. % -target -value))
+                                            :rows 4
+                                            :cols 50}]
+           [:br]
+           [:button.btn.waves-effect.waves-light {:on-click #(try-move-next)}
+            "Let's start improving my life Liz!" [:i.mdi-content-send.right]]]
+          )
+        ]])
+    (if (<= 0 (get-in @prefs [:mood :score]))
+      [:div.mood-good
+       [:h3  "Looks like your mood is good!"]
+       [:button.btn.waves-effect.waves-light {:on-click #(set! (.-location
+                                                                js/window)
+                                                               "#/application")} "Let's start improving my life Liz!" [:i.mdi-content-send.right]]])
+    ]])
